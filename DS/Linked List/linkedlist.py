0579a15c0e3d53087989051e7c4a674b8e49e783
Varnit Mittal <varnitmittal77@gmail.com>
2020-05-08 03:37:13 +0530

    Linked List added

---
+++ DS/Linked List/linkedlist.py
@@ -0,0 +1,150 @@
+# My own implementation of Singly Linked List
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class MyLinkedList:
+    def __init__(self):
+        self.head = None
+
+    def isEmpty(self):
+        if self.head == None:
+            return True
+        else:
+            return False
+
+    def append(self, data):  # adds node to the end of the list
+        if self.isEmpty():
+            self.head = Node(data)
+            return
+        curr = self.head
+        while(curr.next):
+            curr = curr.next
+        curr.next = Node(data)
+
+    def prepend(self, data):  # adds node to the beginning of the list
+        if self.isEmpty():
+            self.head = Node(data)
+            return
+        temp = Node(data)
+        temp.next = self.head
+        self.head = temp
+
+    def addAtIndex(self, index, data):  #adds node somewhere in between the list
+        if self.isEmpty():
+            self.head = Node(data)
+            return
+        curr = self.head
+        for x in range(index-1):  # indexing is same as that of arrays
+            curr = curr.next
+        temp = Node(data)
+        temp.next = curr.next
+        curr.next = temp
+
+    def deleteFirst(self):
+        if self.isEmpty():
+            print("\tUnderflow! List is already empty.")
+            return
+        print("\tThe node deleted is ", self.head.data, " from index 0.")
+        self.head = self.head.next
+
+    def deleteAtIndex(self, index):
+        if self.isEmpty():
+            print("\tUnderflow! List is already empty.")
+            return 
+        elif index >= self.length():
+            print("the element doesn't exist")
+            return
+        else:
+            curr = self.head
+            for x in range(index-1):  # indexing is same as that of arrays
+                curr = curr.next
+            print("\tThe node deleted is ", curr.next.data, " from index ", index)
+            curr.next = curr.next.next
+
+    def length(self):
+        if self.isEmpty():
+            print("The list is empty.")
+            return
+        curr = self.head
+        count = 1
+        while(curr.next):
+            curr = curr.next
+            count += 1
+        return count
+    
+    def findKey(self, key):
+        if self.isEmpty():
+            print("The list is empty. No key found.")
+            return
+        count = 0
+        curr = self.head
+        while(curr.next):
+            if curr.data == key:
+                print("\t", key, " found at index", count)              
+                return
+            curr = curr.next
+            count += 1
+        
+    def display(self):
+        temp = self.head
+        while(temp):
+            print(temp.data, end="  ")
+            temp = temp.next
+        print("\n")
+
+M = MyLinkedList()
+M.head = Node(10)
+second = Node(20)
+third = Node(30)
+
+M.head.next = second
+second.next = third
+
+M.display()
+
+M.append(40)
+M.append(50)
+M.display()
+
+M.prepend(8)
+M.prepend(4)
+M.display()
+
+M.addAtIndex(2, 100)
+M.display()
+
+M.deleteFirst()
+M.display()
+M.deleteAtIndex(3)
+M.display()
+
+print("Length of list is: ", M.length())
+for key in [40, 8]:
+    M.findKey(key)
+
+
+
+
+
+"""OUTPUT
+10  20  30  
+
+10  20  30  40  50  
+
+4  8  10  20  30  40  50  
+
+4  8  100  10  20  30  40  50  
+
+	The node deleted is  4  from index 0.
+8  100  10  20  30  40  50  
+
+	The node deleted is  20  from index  3
+8  100  10  30  40  50  
+
+Length of list is:  6
+	 40  found at index 4
+	 8  found at index 0
+
+"""
--- DS/Queue/deque.py
+++ DS/Queue/deque.py
@@ -1,66 +1,206 @@
-#Deque implementation
+#My own implementation of double-ended circular queue using python list/array
 
-class Deque:
+class MyDeque:
     def __init__(self, *args):
         self.max = 5
-        self.deque = []
-        self.front = -1
-        self. rear = -1
-        self.display()
+        self.deque = list(range(self.max))
 
-    def isFull(self):
-        return False
+        if len(args) > self.max:
+            print("Bounds Violated. Exiting...")
+            exit(0)
+        elif len(args) == 0:
+            self.front = self. rear = - 1  
+            print("Deque of max length {} is ready. \n".format(self.max))
+            self.display()
+        else:
+            for i in range(len(args)):
+                self.deque[i] = list(args)[i]
+            # we put both front and rear as -1 for empty queue
+            # but here the queue is initialized differently
+            self.front = 0
+            self. rear = len(args) - 1  
+            print("DeQue of max length {} is ready. \n".format(self.max))
+            self.display()
     
-    def insertFront(self, x):
+    def isFull(self):
+        if (self.rear + 1) % self.max == self.front:
+            print("\tOverflow!")
+            return True        
+        else:
+            return False
+
+    def enqueueFront(self, x):
         if self.isFull():
-            print("Can't insert, deque is already full.")
-        elif len(self.deque) == 0:
-            self.deque.insert(0, x)
-            self.rear = 0
-            self.front = 0            
-        else:
-            self.deque.insert(0, x)
-            self.front 
+            print("\tCan't add element {} as the deque is full.\n".format(x))        
+        
+        elif self.rear == -1 & self.front == -1:    #no element in queue
+            self.front = self.rear = 0
+            self.deque[self.front] = x
+            print("\n\tAdded element", x, " at index ", self.front)
 
-    def deleteFront(self):
-        if self.isEmpty():
-            print("Deque is already empty")
         else:
-            self.deque.pop(0)
+            if self.front == 0:
+                self.front = self.max - 1    #this is what makes it deque
+                #self.rear = (self.rear + 1) % self.max  
+                self.deque[self.front] = x
+                print("\n\tAdded element", x, " at index ", self.front)
+            else:
+                self.front -= 1
+                self.deque[self.front] = x
+                print("\n\tAdded element", x, " at index ", self.front)
 
+    def enqueueRear(self, x):
+        if self.isFull():
+            print("\tCan't add element {} as the deque is full.\n".format(x))        
+        
+        elif self.rear == -1 & self.front ==-1:    #no element in queue
+            self.front = self.rear = 0
+            self.deque[self.rear] = x
+            print("\n\tAdded element", x, " at index ", self.rear)
+
+        else:
+            if self.rear == self.max - 1:
+                self.rear = 0    #this is what makes it deque
+                #self.rear = (self.rear + 1) % self.max  
+                self.deque[self.rear] = x
+                print("\n\tAdded element", x, " at index ", self.rear)
+            else:
+                self.rear += 1
+                self.deque[self.rear] = x
+                print("\n\tAdded element", x, " at index ", self.rear)
+    
     def isEmpty(self):
-        return False
-
-    def insertRear(self, x):
-        if self.isFull():
-            print("Can't insert, deque is already full.")
+        if (self.front == -1 & self.rear == -1):
+            print("\tUnderflow")
+            return True       
         else:
-            self.deque.append(x)
-
-    def deleteRear(self):
+            return False
+    
+    def dequeueFront(self):
         if self.isEmpty():
-            print("Deque is already empty")
+            print("\tCan't delete element as the deque is empty.")
+        
+        elif self.rear == self.front:    #one element in queue
+            print("\t", self.deque[self.front], "is deleted from index ", self.front," The queue is empty now")
+            self.front = self.rear = -1 #no element is left now
+
         else:
-            self.deque.pop()
+            if self.front == self.max - 1:
+                print("\t", self.deque[self.front], "is deleted from index ", self.front)
+                self.front = 0  #this is what makes it circular
+            else:
+                print("\t", self.deque[self.front], "is deleted from index ", self.front)
+                self.front += 1
 
-    def peekFront(self):
-        pass
+    def dequeueRear(self):
+        if self.isEmpty():
+            print("\tCan't delete element as the deque is empty.")
+        
+        elif self.rear == self.front:    #one element in queue
+            print("\t", self.deque[self.rear], "is deleted from index ", self.rear," The queue is empty now")
+            self.front = self.rear = -1 #no element is left now
 
-    def peekRear(self):
-        pass
+        else:
+            if self.rear == 0:
+                print("\t", self.deque[self.rear], "is deleted from index ", self.rear)
+                self.rear = self.max - 1  #this is what makes it circular
+            else:
+                print("\t", self.deque[self.rear], "is deleted from index ", self.rear)
+                self.rear -= 1
 
     def display(self):
-        print(self.deque)
+        print("\nDISPLAY:")     
+        if self.isEmpty():
+            print("The queue is empty, nothing to display.")
+            print("front is at index ", self.front)
+            print("rear is at index ", self.rear)
+        
+        elif self.rear >= self.front:
+            print("The deque is ") 
+            for i in range(self.front, self.rear + 1): 
+                print(self.deque[i], end = "  ")
+            print("\nfront is at index ", self.front)
+            print("rear is at index ", self.rear) 
+        
+        else:    
+            print("The deque is ")
+            for i in range(0, self.rear + 1): 
+                print(self.deque[i], end = " ")
+            for i in range(self.front, self.max): 
+                print(self.deque[i], end = " ") 
+            print("\nfront is at index ", self.front)
+            print("rear is at index ", self.rear)
 
-D = Deque()
-D.insertFront(10)
-D.insertRear(20)
-D.insertRear(30)
-D.insertRear(40)
-D.insertRear(50)
-D.insertRear(60)
-D.insertRear(70)
-D.display()
-D.deleteRear()
-D.deleteFront()
-D.display()
+C = MyDeque()
+
+C.enqueueFront(10)
+C.enqueueRear(100)
+C.enqueueFront(20)
+C.enqueueRear(90)
+C.enqueueRear(80)
+C.enqueueFront(30)
+C.display()
+
+C.dequeueRear()
+C.dequeueFront()
+C.dequeueRear()
+C.display()
+
+C.dequeueRear()
+C.dequeueFront()
+C.dequeueRear()
+C.display()
+
+
+
+
+
+"""OUTPUT
+Deque of max length 5 is ready. 
+
+
+DISPLAY:
+	Underflow
+The queue is empty, nothing to display.
+front is at index  -1
+rear is at index  -1
+
+	Added element 10  at index  0
+
+	Added element 100  at index  1
+
+	Added element 20  at index  4
+
+	Added element 90  at index  2
+
+	Added element 80  at index  3
+	Overflow!
+	Can't add element 30 as the deque is full.
+
+
+DISPLAY:
+The deque is 
+10 100 90 80 20 
+front is at index  4
+rear is at index  3
+	 80 is deleted from index  3
+	 20 is deleted from index  4
+	 90 is deleted from index  2
+
+DISPLAY:
+The deque is 
+10  100  
+front is at index  0
+rear is at index  1
+	 100 is deleted from index  1
+	 10 is deleted from index  0  The queue is empty now
+	Underflow
+	Can't delete element as the deque is empty.
+
+DISPLAY:
+	Underflow
+The queue is empty, nothing to display.
+front is at index  -1
+rear is at index  -1
+
+"""
